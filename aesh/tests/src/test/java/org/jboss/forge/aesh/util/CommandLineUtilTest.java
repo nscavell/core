87285dd3095668dd56cfe47e06058dfb2b22ac53
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-05-03 00:34:56 -0400

    Test is close to passing

--- aesh/tests/src/test/java/org/jboss/forge/aesh/util/CommandLineUtilTest.java
+++ aesh/tests/src/test/java/org/jboss/forge/aesh/util/CommandLineUtilTest.java
@@ -9,12 +9,23 @@ package org.jboss.forge.aesh.util;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import javax.inject.Inject;
+
 import org.jboss.aesh.cl.CommandLine;
 import org.jboss.aesh.cl.CommandLineParser;
 import org.jboss.aesh.cl.OptionBuilder;
 import org.jboss.aesh.cl.exception.CommandLineParserException;
 import org.jboss.aesh.cl.internal.ParameterInt;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.aesh.ForgeShell;
 import org.jboss.forge.aesh.ShellContext;
+import org.jboss.forge.arquillian.Addon;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.container.addons.AddonId;
+import org.jboss.forge.container.addons.AddonRegistry;
+import org.jboss.forge.container.repositories.AddonDependencyEntry;
 import org.jboss.forge.ui.UICommand;
 import org.jboss.forge.ui.context.UIBuilder;
 import org.jboss.forge.ui.context.UIContext;
@@ -24,20 +35,49 @@ import org.jboss.forge.ui.input.UIInput;
 import org.jboss.forge.ui.metadata.UICommandMetadata;
 import org.jboss.forge.ui.result.Result;
 import org.jboss.forge.ui.util.Metadata;
-import org.junit.Ignore;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author <a href="mailto:stale.pedersen@jboss.org">Ståle W. Pedersen</a>
  */
-@Ignore
+@RunWith(Arquillian.class)
 public class CommandLineUtilTest
 {
+
+   @Deployment
+   @Dependencies({ @Addon(name = "org.jboss.forge:ui", version = "2.0.0-SNAPSHOT"),
+            @Addon(name = "org.jboss.forge:aesh-test-harness", version = "2.0.0-SNAPSHOT"),
+            @Addon(name = "org.jboss.forge:aesh", version = "2.0.0-SNAPSHOT"),
+            @Addon(name = "org.jboss.forge:resources", version = "2.0.0-SNAPSHOT")
+   })
+   public static ForgeArchive getDeployment()
+   {
+      ForgeArchive archive = ShrinkWrap
+               .create(ForgeArchive.class)
+               .addBeansXML()
+               .addAsAddonDependencies(
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge:ui", "2.0.0-SNAPSHOT")),
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge:aesh", "2.0.0-SNAPSHOT")),
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge:aesh-spi", "2.0.0-SNAPSHOT")),
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge:aesh-test-harness", "2.0.0-SNAPSHOT")),
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge:resources", "2.0.0-SNAPSHOT"))
+               );
+
+      return archive;
+   }
+
+   @Inject
+   private ForgeShell shell;
+
+   @Inject
+   private AddonRegistry registry;
 
    @Test
    public void testGenerateParser() throws Exception
    {
-      ShellContext context = new ShellContext(null);
+      ShellContext context = new ShellContext(shell);
       Foo1Command foo1 = new Foo1Command();
       foo1.initializeUI(context);
       CommandLineParser parser = CommandLineUtil.generateParser(foo1, context);
@@ -62,7 +102,7 @@ public class CommandLineUtilTest
       UIInput<Integer> input2 = new UIInputImpl<Integer>("int", Integer.class);
       UIInput<Boolean> input3 = new UIInputImpl<Boolean>("bool", Boolean.class);
 
-      ShellContext context = new ShellContext(null);
+      ShellContext context = new ShellContext(shell);
       context.add(input1);
       context.add(input2);
       context.add(input3);
@@ -75,20 +115,20 @@ public class CommandLineUtilTest
       CommandLineParser clp = new CommandLineParser(param);
       CommandLine cl = clp.parse("test --str yay");
 
-      CommandLineUtil.populateUIInputs(cl, context, null);
+      CommandLineUtil.populateUIInputs(cl, context, registry);
       assertEquals(input1.getValue(), "yay");
       assertNull(input2.getValue());
       assertNull(input3.getValue());
 
       cl = clp.parse("test --str yay --int 10");
-      CommandLineUtil.populateUIInputs(cl, context, null);
+      CommandLineUtil.populateUIInputs(cl, context, registry);
       assertEquals(input1.getValue(), "yay");
       assertEquals(input2.getValue(), new Integer(10));
       assertNull(input3.getValue());
 
-      cl = clp.parse("test --bool");
-      CommandLineUtil.populateUIInputs(cl, context, null);
-      assertEquals(input3.getValue(), Boolean.TRUE);
+      cl = clp.parse("test --bool true");
+      CommandLineUtil.populateUIInputs(cl, context, registry);
+      assertEquals(Boolean.TRUE, input3.getValue());
       assertNull(input1.getValue());
       assertNull(input2.getValue());
    }
@@ -102,7 +142,7 @@ public class CommandLineUtilTest
       CommandLineParser clp = CommandLineUtil.generateParser(foo2, context);
 
       CommandLine cl = clp.parse("foo2 --str yay");
-      CommandLineUtil.populateUIInputs(cl, context, null);
+      CommandLineUtil.populateUIInputs(cl, context, registry);
 
       assertEquals("yay", ((UIInput<?>) context.findInput("str")).getValue());
    }
